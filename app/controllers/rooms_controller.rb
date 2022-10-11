272adebc465fb01e8a9045ab9e7b0313b012613f
psarge2 <paul@psarge.dev>
2022-10-11 14:15:30 -0400

    created a room index, show and new page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,21 @@
 class RoomsController < ApplicationController
+before_action :set_room, only: %i[show]
+
+  def index
+    @rooms = Room.all
+  end
+
+  def show
+  end
+
+  def new
+    @room = Room.new
+  end
+
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
 end
